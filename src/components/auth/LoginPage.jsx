f8df332b37ec69be8c4624bb74301d8bff0e4773
Ryan James C. Pancho <rcpancho@sscgi.com>
2026-01-19 11:06:38 +0800

    Replaced placeholder title and removed in-battle anims

--- src/components/auth/LoginPage.jsx
+++ src/components/auth/LoginPage.jsx
@@ -95,7 +95,7 @@ export default function LoginPage() {
   return (
     <div className="page-wrapper">
       <div className="login-container">
-        <h1>PayPal</h1>
+        <h1>Pokemon</h1>
         <h2>Sign in</h2>
 
         {isVerifyingWallet && (
